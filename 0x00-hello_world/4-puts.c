9875075fefabf5c65c96d1f7bdc2126c25150c7a
Blazeker <1687@holbertonschool.com>
2020-02-05 13:28:54 -0500

    C Basics

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-/* Print an message */
+/**
+ * main - Entry point
+ * puts - Print the message that we enter
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
