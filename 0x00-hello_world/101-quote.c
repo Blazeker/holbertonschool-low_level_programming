de588d989c9aded46c2bcb6ec5e9d064e9c178d9
Blazeker <1687@holbertonschool.com>
2020-02-05 15:40:41 -0500

    C Basics

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
+ * printf - to print the quote
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf ("and that piece of art is useful\" - Dora Korpar, 2015-10-19""\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19""\n", 58);
 return (1);
 }
