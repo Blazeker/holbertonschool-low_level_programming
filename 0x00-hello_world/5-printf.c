a3f8ff575db8f8d61f96f77b59d1fae610378669
Blazeker <1687@holbertonschool.com>
2020-02-05 13:32:25 -0500

    C Basics

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * printf - Print things with so many options of printing
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
