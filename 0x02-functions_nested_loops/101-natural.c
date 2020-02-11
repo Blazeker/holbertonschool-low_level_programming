aed33806e663fbcab1c7a2dcdf219293993be68b
Blazeker <1687@holbertonschool.com>
2020-02-11 13:52:26 -0500

    C Functions advance

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * natural_numbers - Print all multiples of 3 and 5 until 1024
+ */
+
+void natural_numbers(void)
+{
+	int c;
+	int sum = 0;
+
+	for (c = 3; c < 1024; c++)
+	{
+		if (c % 3 == 0 || c % 5 == 0)
+		{
+		sum = c + sum;
+		}
+	}
+	printf("%d", sum);
+}
