c1e4813fb1847269cb35389395cd6ba639849f20
Blazeker <1687@holbertonschool.com>
2020-02-11 14:13:59 -0500

    C Functions advance

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * fibonacci50 - Print the 50 first numbers of fibonacci
+ */
+
+void fibonacci50(void)
+{
+	long int a = 1;
+	long int b = 2;
+	long int sum;
+	int cont = 0;
+
+	while (cont < 48)
+	{
+		sum = a + b;
+		if (cont == 47)
+		{
+		printf("%li", sum);
+		}
+		else
+		{
+		printf("%li%s", sum, ", ");
+		a = b;
+		b = sum;
+		}
+		cont++;
+	}
+	printf("\n");
+}
