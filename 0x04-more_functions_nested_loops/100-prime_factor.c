61b07d8db51aa44a04d212223cec6dc30958af6b
Blazeker <1687@holbertonschool.com>
2020-02-13 14:19:22 -0500

    More functions of c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int n = 612852475143;
+	int div = 2;
+	int s = 1;
+
+	while (s != 0)
+	{
+		if (n % div != 0)
+		{
+			div = div + 1;
+		}
+		else
+		{
+			n = n / div;
+			if (n == 1)
+			{
+				printf("The largest prime factor is: %d", div);
+				s = 0;
+			}
+		}
+	}
+	printf("\n");
+}
