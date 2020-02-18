f33b6444680b7bfc8fa4a496e8bd0c0cc9d58fb7
Blazeker <1687@holbertonschool.com>
2020-02-18 12:07:27 -0500

    Pointers and arrays

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,16 +9,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int cont = 0;
 	int i = 0;
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	for (; i < cont; i++)
-	{
 		dest[i] = '\0';
-	}
 	return (dest);
 }
