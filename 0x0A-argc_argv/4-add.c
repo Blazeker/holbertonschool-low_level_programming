a9a2ba1fc90b60d569a287774e6375b895d792f5
Blazeker <60364105+Blazeker@users.noreply.github.com>
2020-02-29 13:44:56 -0500

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "holberton.h"
 /**
  * main - Entry Point
  * @argc: arguments
  * @argv: array pointing to arguments
  * Return: 0
  */
-
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
