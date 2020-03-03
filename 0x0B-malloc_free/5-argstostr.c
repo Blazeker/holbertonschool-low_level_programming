d30ea913bd7a6fe0a80ffabfab3fc8c3e84a9cfd
Blazeker <1687@holbertonschool.com>
2020-03-03 15:51:32 -0500

    Malloc

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - A function
+ * @ac : arguments
+ * @av : an array
+ * Return: an array
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, len = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			len++;
+		}
+	}
+	len += ac;
+	s = malloc(sizeof(char) * len + 1);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			s[k] = av[i][j];
+			k++;
+		}
+		if (s[k] == '\0')
+		{
+			s[k++] = '\n';
+		}
+	}
+	return (s);
+}
