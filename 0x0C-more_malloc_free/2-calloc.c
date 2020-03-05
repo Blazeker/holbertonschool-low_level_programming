58d9eab2c65d28d316ce70dd3824b06dae17dce3
Blazeker <1687@holbertonschool.com>
2020-03-05 13:27:19 -0500

    MOre malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,17 +10,17 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *s;
+	char *s;
 	unsigned int i = 0;
 
-	if (nmemb < 1 || size < 1)
+	if (!nmemb || !size)
 		return (0);
 	s = malloc(nmemb * size);
 	if (s == 0)
 		return (0);
 	while (i < nmemb * size)
 	{
-		s[i] = '0';
+		s[i] = 0;
 		i++;
 	}
 	return (s);
