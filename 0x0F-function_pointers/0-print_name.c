407626a4d65e6a648d4de129b0ab9b99a82ce450
Blazeker <1687@holbertonschool.com>
2020-03-11 14:45:46 -0400

    Function to pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,13 +1,9 @@
 #include "function_pointers.h"
-
 /**
 * print_name - Print the name
 * @name : The name
 * @f : the pointer
-* Description: hace cosas muy buenas
-* Return : 0
 */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (!name || !f)
