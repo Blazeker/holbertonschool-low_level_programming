25440e3d1a03c69c1c99a3a7b456dce21c5c4399
Blazeker+ <BlackDiseasex@gmail.com>
2020-03-24 19:56:42 +0000

    Fixed

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,6 +13,8 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	new = *head;
 	*head = malloc(sizeof(listint_t));
+	if (!*head)
+		return (NULL);
 	(*head)->n = n;
 	(*head)->next = new;
 	return (*head);
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,6 +18,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 			temp = temp->next;
 	}
 	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
 	new->n = n;
 	if (temp)
 		temp->next = new;
