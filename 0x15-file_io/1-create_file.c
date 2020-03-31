5b38ca72e20f340ac39e2de1c6a5d2c1083acd7e
Blazeker+ <BlackDiseasex@gmail.com>
2020-03-31 17:26:13 +0000

    File I/O

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,12 +18,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (text_content)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
-
-	while (*(text_content + len))
-		len++;
 	err = write(fd, text_content, len);
 	close(fd);
 	if (err < 0)
