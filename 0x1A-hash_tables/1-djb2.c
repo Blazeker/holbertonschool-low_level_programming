5bee34c367089e6775245ab738f66acb9a2fc588
Blazeker <1687@holbertonschool.com>
2020-05-29 15:44:50 +0000

    Fixed

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,3 +1,9 @@
+/**
+ * hash_djb2 - Hash function implementing the djb2.
+ * @str: The string to hash.
+ * Return: The calculated hash.
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
