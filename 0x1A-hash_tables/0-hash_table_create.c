5a8e30f8dea2cec4b3bef35c9b39f20f236e6989
Blazeker <1687@holbertonschool.com>
2020-05-29 15:43:41 +0000

    Fixed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,20 +9,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *ht;
 	unsigned long int i;
-	hash_table_t *new_table = NULL;
 
-	new_table = malloc(sizeof(hash_table_t));
-	if (!new_table)
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
-	new_table->size = size;
-	new_table->array = malloc(sizeof(hash_node_t) * size);
-	if (!new_table->array)
-	{
-		free(new_table);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
-	}
 	for (i = 0; i < size; i++)
-		(new_table->array)[i] = NULL;
-	return (new_table);
+		ht->array[i] = NULL;
+
+	return (ht);
 }
